052fab445efd3c61dbb258e7ee98765bd00fa104
Johan Diaz <158389925+ssh0annn@users.noreply.github.com>
2025-12-17 15:45:27 -0400

    Delete userviews/userviews directory

--- userviews/userviews/src/main/java/com/orionteck/userviews/command/CrearDireccionesCommnad.java
+++
@@ -1,9 +0,0 @@
-package com.orionteck.userviews.command;
-
-//Consulta para listados o vistas resumidas
-public record CrearDireccionesCommnad(
-         String calle,
-         String ciudad,
-         String pais
-) {
-}
--- userviews/userviews/src/main/java/com/orionteck/userviews/query/DireccionQuery.java
+++
@@ -1,9 +0,0 @@
-package com.orionteck.userviews.query;
-
-public record DireccionQuery(
-        Long id,
-        String Calle,
-        String ciudad,
-        String pais
-) {
-}
